64f5bcf9427e1ed1da039f8a3a84f33f6126bbf1
Tayyyyylor <bryan.h.pro@gmail.com>
2024-12-24 10:33:04 +0100

    imrovments of the homepage

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import createNextIntlPlugin from 'next-intl/plugin'
+import { withNextVideo } from 'next-video/process'
 import type { NextConfig } from 'next'
 
 const withNextIntl = createNextIntlPlugin()
@@ -9,5 +10,6 @@ const nextConfig: NextConfig = {
         domains: ['d2pq7mh0qct8hv.cloudfront.net'],
     },
 }
+const combinedConfig = withNextVideo(withNextIntl(nextConfig))
 
-export default withNextIntl(nextConfig)
+export default combinedConfig
