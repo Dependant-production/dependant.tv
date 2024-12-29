623462d36a1aa43d6c4c5e794933d1a6996913d0
Tayyyyylor <bryan.h.pro@gmail.com>
2024-12-29 17:15:59 +0100

    add compteur sur page d'accueuil

--- src/components/templates/homepage/Homepage.tsx
+++ src/components/templates/homepage/Homepage.tsx
@@ -3,31 +3,35 @@
 import React, { useEffect, useState } from 'react'
 import styles from './Homepage.module.scss'
 import { Link } from '@/i18n/routing'
+import CounterSlide from '@/components/molecules/counterSlide/CounterSlide'
 
 export default function Homepage({ homepageData }: any) {
     const [currentVideo, setCurrentVideo] = useState<string | null>(null)
     const [currentTitle, setCurrentTitle] = useState('')
     const [currentDirector, setCurrentDirector] = useState('')
     const [currentIndex, setCurrentIndex] = useState(0)
+    const sortedData = React.useMemo(() => {
+        return [...homepageData].sort((a, b) => a.order - b.order)
+    }, [homepageData])
 
     console.log('homepageData', homepageData)
 
     useEffect(() => {
-        if (!homepageData || homepageData.length === 0) return
-        const video = homepageData[currentIndex]
+        if (!homepageData || sortedData.length === 0) return
+        const video = sortedData[currentIndex]
         const videoUrl = video?.url?.[0]?.url
         setCurrentVideo(videoUrl as string)
         setCurrentTitle(video?.title || '')
         setCurrentDirector(video?.director?.name || '')
         // Changer de vidéo toutes les 5 secondes
         const interval = setInterval(() => {
-            const nextIndex = (currentIndex + 1) % homepageData.length
+            const nextIndex = (currentIndex + 1) % sortedData.length
             setCurrentIndex(nextIndex)
         }, 10000)
 
         // Nettoyer l'intervalle au démontage
         return () => clearInterval(interval)
-    }, [currentIndex, homepageData])
+    }, [currentIndex, sortedData, homepageData])
 
     const getLinkDirectors = () => {
         let link
@@ -59,6 +63,11 @@ export default function Homepage({ homepageData }: any) {
                     playsInline
                 />
             </section>
+            <CounterSlide
+                className={styles.counter}
+                data={sortedData}
+                index={currentIndex}
+            />
         </main>
     )
 }
