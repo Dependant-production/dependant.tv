152a5ad35189e845ef0a0cfcf73da1b7cf71b72d
Tayyyyylor <bryan.h.pro@gmail.com>
2024-12-29 11:58:26 +0100

    add maintenance page

---
+++ src/app/maintenance/page.tsx
@@ -0,0 +1,15 @@
+export default function Maintenance() {
+    return (
+        <html>
+            <body>
+                <main style={{ textAlign: 'center', padding: '50px' }}>
+                    <h1>Maintenance en cours</h1>
+                    <p>
+                        Notre site est actuellement en maintenance. Revenez
+                        bientôt !
+                    </p>
+                </main>
+            </body>
+        </html>
+    )
+}
