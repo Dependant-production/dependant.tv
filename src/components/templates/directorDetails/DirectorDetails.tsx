902d847b425b3ff63b714138625cd3d5c6127a5b
Tayyyyylor <bryan.h.pro@gmail.com>
2025-01-05 20:16:44 +0100

    test pour erreur video en mobile

--- src/components/templates/directorDetails/DirectorDetails.tsx
+++ src/components/templates/directorDetails/DirectorDetails.tsx
@@ -132,7 +132,6 @@ export default function DirectorDetails({ directorData }: any) {
                             <video
                                 src={currentVideoUrl}
                                 controls
-                                autoPlay
                                 controlsList="nodownload"
                                 className={styles.videoPlayer}
                             />
