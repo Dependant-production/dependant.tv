356f99ad35c7f0d9c3c78d677fffd47bfb6ec5e5
Tayyyyylor <bryan.h.pro@gmail.com>
2025-01-17 10:28:27 +0100

    mini anim on loader

--- src/components/molecules/loader/Loader.tsx
+++ src/components/molecules/loader/Loader.tsx
@@ -1,11 +1,27 @@
 'use client'
+import React, { useRef } from 'react'
+import gsap from 'gsap'
 import Image from 'next/image'
-import React from 'react'
 import styles from './Loader.module.scss'
+import { useGSAP } from '@gsap/react'
 
 export default function Loader() {
+    const containerRef = useRef<HTMLDivElement | null>(null)
+
+    useGSAP(() => {
+        gsap.fromTo(
+            containerRef.current,
+            {
+                opacity: 0,
+            },
+            {
+                opacity: 1,
+                duration: 0.2,
+            }
+        )
+    })
     return (
-        <main className={styles.loader}>
+        <main className={styles.loader} ref={containerRef}>
             <Image
                 src="/logoDpdBlack.png"
                 alt="loader"
